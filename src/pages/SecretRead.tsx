2cc38236933d071f3ae7efca2303f0e4b410842d
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-07 12:30:08 +0200

    added SecretRead page and style

--- src/pages/SecretRead.tsx
+++ src/pages/SecretRead.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 
 export default function SecretRead() {
   return (
-    <div>SecretRead</div>
+    <main id="page-secret-read" className="flex-center-col">
+      <section className="text-center">
+        <h1>Read secret</h1>
+        <p>Give the key of a secret and read its message, if the secret has not been expired yet.</p>
+
+        <form className="block-filled flex-center-col">
+          <input className="input" type="text" name="hash" placeholder="Key" required={true} />
+
+          <button className="btn-primary">
+            Read secret
+          </button>
+        </form>
+      </section>
+    </main>
   )
 }
