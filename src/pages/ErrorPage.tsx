11264af1324709793accd2d39c7abbbc0917e301
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-07 11:46:15 +0200

    removed errorpage style include added #page-error id to <main>

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../scss/pages/errorpage.scss';
 
 type ErrorPageProps = {
   code?: number
@@ -7,7 +6,7 @@ type ErrorPageProps = {
 
 export default function ErrorPage({ code = 404 }: ErrorPageProps) {
   return (
-    <main>
+    <main id="page-error">
       <section className="text-center">
         <h1>{code}</h1>
         {code === 404 && <p>Ooops, something went wrong...</p>}
