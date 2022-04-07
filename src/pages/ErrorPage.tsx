073e43888dd5a4f204446510fb4f9b759d4dbc2b
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-07 12:00:31 +0200

    added flex-center-col to main element in ErrorPage component

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -6,7 +6,7 @@ type ErrorPageProps = {
 
 export default function ErrorPage({ code = 404 }: ErrorPageProps) {
   return (
-    <main id="page-error">
+    <main id="page-error" className="flex-center-col">
       <section className="text-center">
         <h1>{code}</h1>
         {code === 404 && <p>Ooops, something went wrong...</p>}
