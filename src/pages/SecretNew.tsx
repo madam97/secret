1df1035b286a22a48c026c3746137589df24eda8
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-07 12:17:38 +0200

    added SecretNew page and style

--- src/pages/SecretNew.tsx
+++ src/pages/SecretNew.tsx
@@ -2,6 +2,20 @@ import React from 'react';
 
 export default function SecretNew() {
   return (
-    <div>SecretNew</div>
+    <main id="page-secret-new" className="flex-center-col">
+      <section className="text-center">
+        <h1>Add new secret</h1>
+        <p>Give a message and the secret's life time and you will recieve the key to read the secret message.</p>
+
+        <form className="block-filled flex-center-col">
+          <input className="input" type="text" placeholder="Message" required={true} />
+          <input className="input" type="number" placeholder="Life time in seconds" min={0} value={0} />
+
+          <button className="btn-primary">
+            Add secret
+          </button>
+        </form>
+      </section>
+    </main>
   )
 }
