6a1c53fd0955959095a79026c8e5876cab322dde
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-07 11:13:45 +0200

    added router usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SecretAdd from './pages/SecretAdd';
+import SecretGet from './pages/SecretGet';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
     <div className="App">
-      Working
+      <Switch>
+        <Route path="/" exact={true}>
+          <Home />
+        </Route>
+        <Route path="/secret/add">
+          <SecretAdd />
+        </Route>
+        <Route path="/secret/get">
+          <SecretGet />
+        </Route>
+        <Route path="*">
+          <ErrorPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
